f712f776a90e29217e1dfd8b4aef592d10b504e5
Karan Kumar <karankumar0334@gmail.com>
2023-05-11 23:03:17 +0500

    Initial Setup

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import * as React from 'react';
+
+export default function Home() {
+
+  return (
+    <View className='bg-purple-500 items-center justify-center'>
+      <Text>Home</Text>
+    </View>
+  );
+
+}
