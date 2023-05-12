116fac55b2d82af53688486a10c946d781ea1610
Karan Kumar <karankumar0334@gmail.com>
2023-05-12 17:14:05 +0500

    Searchbar UI added

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,12 +1,61 @@
-import { View, Text } from 'react-native';
-import * as React from 'react';
+import { View, Text, Button, Pressable, Image, TextInput, StyleSheet } from "react-native";
+import * as React from "react";
+import Icon from "react-native-vector-icons/Ionicons";
 
 export default function Home() {
 
+  const [searchBarText, setSearchBarText] = React.useState("");
+
+  const handleSearchSubmit = async () => {
+    console.log(searchBarText);
+  };
+
   return (
-    <View className='bg-purple-500 items-center justify-center'>
-      <Text>Home</Text>
+    <View style={styles.homeScreen}>
+
+      {/* SearchBar */}
+      <View style={styles.searchBarContainer}>
+        <TextInput
+          defaultValue={searchBarText}
+          onChangeText={(newText) => { setSearchBarText(newText); }}
+          style={styles.searchBarInput}
+          placeholder="Search games"
+          placeholderTextColor={'rgba(255, 255, 255, 0.4)'}
+        />
+        <Pressable onPress={handleSearchSubmit}>
+          <Icon
+            name="ios-search"
+            color="#e7e8e8"
+            size={24}
+          />
+        </Pressable>
+      </View>
+
     </View>
   );
-
 }
+
+const styles = StyleSheet.create({
+
+  homeScreen: {
+    backgroundColor: '#0B1416',
+    // flex: '1 1 0%';
+    flexGrow: 1
+  },
+
+  searchBarContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    backgroundColor: '#232c2d',
+    padding: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: '#3c4345'
+  },
+
+  searchBarInput: {
+    fontSize: 20,
+    color: '#e7e8e8',
+  },
+
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: '#0B1416',
+        primary: '#64A36F',
+        'primary-light': '#CEF0B9',
+        'accent-contrast': '#FFF'
+      }
+    },
   },
   plugins: [],
 };
